6142360e443e54e6b69ed7538a0d64e2a2164a63
Daniel Bergey <daniel.bergey@braze.com>
2024-08-19 18:42:17 -0400

    rdkafka producer, with tokio

--- src/bin/producer.rs
+++ src/bin/producer.rs
@@ -1,43 +1,42 @@
 /// read from stdin, send each line to Kafka
-use kafka::client::KafkaClient;
-use kafka::producer::{Producer, Record, RequiredAcks};
 use std::env;
 use std::io;
 use std::io::BufRead;
 use std::time::Duration;
 use anyhow::Result;
 
-fn main() -> Result<()> {
+use rdkafka::config::ClientConfig;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+
+#[tokio::main]
+async fn main() -> Result<()> {
     // let port = env::var("PORT").unwrap_or("3003".to_string());
     // let listen_ip = env::var("LISTEN_IP").unwrap_or("0.0.0.0".to_string());
     let broker_url = env::var("BROKER_URL").unwrap_or("localhost:9092".to_string());
     let topic = env::var("TOPIC").unwrap_or("buffer-topic".to_string());
 
-    let mut client = KafkaClient::new(vec![broker_url.clone()]);
-    client.load_metadata_all()?;
-    println!(
-        "topics: {}",
-        client
-            .topics()
-            .names()
-            .map(|s| s.to_owned())
-            .collect::<Vec<String>>()
-            .join(", ")
-    );
+    // Create the `FutureProducer` to produce asynchronously.
+    let producer: FutureProducer = ClientConfig::new()
+        .set("bootstrap.servers", &broker_url)
+        .set("message.timeout.ms", "1000")
+        .create()?;
 
-    let mut producer = Producer::from_hosts(vec![broker_url])
-        .with_ack_timeout(Duration::from_secs(1))
-        .with_required_acks(RequiredAcks::One)
-        .create()
-        .unwrap();
-
+    // no http server yet, so read messages from stdin
     let stdin = io::stdin();
     let in_handle = stdin.lock();
     for r_line in in_handle.lines() {
         let line = r_line?;
-        producer
-            .send(&Record::from_value(&topic, line.as_bytes()))
-            .unwrap();
+
+        let produce_future = producer.send(
+            FutureRecord::<(), String>::to(&topic)
+                // .key("some key")
+                .payload(&line),
+            Duration::from_secs(0),
+        );
+        match produce_future.await {
+            Ok(delivery) => println!("Sent: {:?}", delivery),
+            Err((e, _)) => println!("Error: {:?}", e),
+        }
     }
     Ok(())
 }
