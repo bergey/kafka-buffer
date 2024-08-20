4b74c33fdda6bbb983a1605844d250dcd5b19638
Daniel Bergey <daniel.bergey@braze.com>
2024-08-19 22:36:16 -0400

    introduce hyper, write the http payload to kafka

--- src/bin/producer.rs
+++ src/bin/producer.rs
@@ -1,42 +1,88 @@
 /// read from stdin, send each line to Kafka
 use std::env;
-use std::io;
-use std::io::BufRead;
 use std::time::Duration;
-use anyhow::Result;
 
 use rdkafka::config::ClientConfig;
 use rdkafka::producer::{FutureProducer, FutureRecord};
 
+use http_body_util::{BodyExt, Empty};
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+// use tokio::net::TcpStream;
+
+#[derive(Clone, Debug)]
+struct Config {
+    broker_url: String,
+    topic: String,
+    request_max_size: usize,
+}
+
 #[tokio::main]
-async fn main() -> Result<()> {
-    // let port = env::var("PORT").unwrap_or("3003".to_string());
-    // let listen_ip = env::var("LISTEN_IP").unwrap_or("0.0.0.0".to_string());
-    let broker_url = env::var("BROKER_URL").unwrap_or("localhost:9092".to_string());
-    let topic = env::var("TOPIC").unwrap_or("buffer-topic".to_string());
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let listen: SocketAddr = env::var("LISTEN")
+        .ok()
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(SocketAddr::from(([0, 0, 0, 0], 3000)));
+    let config: &'static Config = Box::leak(Box::new(Config {
+        broker_url: env::var("BROKER_URL").unwrap_or("localhost:9092".to_string()),
+        topic: env::var("TOPIC").unwrap_or("buffer-topic".to_string()),
+        request_max_size: env::var("REQUEST_MAX_SIZE")
+            .ok()
+            .and_then(|s| s.parse().ok())
+            .unwrap_or(2 ^ 20),
+    }));
 
-    // Create the `FutureProducer` to produce asynchronously.
-    let producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", &broker_url)
-        .set("message.timeout.ms", "1000")
-        .create()?;
+    let write_to_kafka = move |req: Request<hyper::body::Incoming>| {
+        let req = http_body_util::Limited::new(req.into_body(), config.request_max_size);
+        async {
+            match req.collect().await {
+                Ok(all) => {
+                    // Create the `FutureProducer` to produce asynchronously.
+                    let producer: FutureProducer = ClientConfig::new()
+                        .set("bootstrap.servers", &config.broker_url)
+                        .set("message.timeout.ms", "1000")
+                        .create()?;
 
-    // no http server yet, so read messages from stdin
-    let stdin = io::stdin();
-    let in_handle = stdin.lock();
-    for r_line in in_handle.lines() {
-        let line = r_line?;
-
-        let produce_future = producer.send(
-            FutureRecord::<(), String>::to(&topic)
-                // .key("some key")
-                .payload(&line),
-            Duration::from_secs(0),
-        );
-        match produce_future.await {
-            Ok(delivery) => println!("Sent: {:?}", delivery),
-            Err((e, _)) => println!("Error: {:?}", e),
+                    let mut v: Vec<u8> = Vec::new();
+                    v.extend(all.to_bytes().as_ref());
+                    let produce_future = producer.send(
+                        FutureRecord::<(), [u8]>::to(&config.topic)
+                            .payload(&v),
+                        Duration::from_secs(0),
+                    );
+                    match produce_future.await {
+                        Ok(delivery) => println!("Sent: {:?}", delivery),
+                        Err((e, _)) => println!("Error: {:?}", e),
+                    }
+                }
+                Err(err) => println!("error: {:?}", err)
+            }
+            Ok::<Response<Empty<Bytes>>, anyhow::Error>(Response::new(Empty::<Bytes>::new()))
         }
+    };
+
+    let listener = { TcpListener::bind(listen).await? };
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(write_to_kafka))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
     }
-    Ok(())
 }
