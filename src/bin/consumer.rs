f66ae9706d277aaa839caa337275186a5e98c3e2
Daniel Bergey <daniel.bergey@braze.com>
2024-08-20 06:00:34 -0400

    consume from Kafka, write to Sidekiq

--- src/bin/consumer.rs
+++ src/bin/consumer.rs
@@ -4,11 +4,13 @@ use std::env;
 use rdkafka::config::ClientConfig;
 use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::consumer::Consumer;
+use rdkafka::Message;
+use sidekiq::{create_redis_pool, Client, Job, JobOpts};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let kafka_url = env::var("KAFKA_URL").unwrap_or("localhost:9092".to_string());
-    let topic = env::var("TOPIC").unwrap_or("buffer-topic".to_string());
+    let topic = Box::leak(Box::new(env::var("TOPIC").unwrap_or("buffer-topic".to_string())));
 
     // Create the `StreamConsumer`, to receive the messages from the topic in form of a `Stream`.
     let consumer: StreamConsumer = ClientConfig::new()
@@ -20,11 +22,45 @@ async fn main() -> anyhow::Result<()> {
         .create()?;
     consumer.subscribe(&[&topic])?;
 
+    let redis_pool = create_redis_pool()?;
+    let sidekiq_client = Client::new(redis_pool, Default::default());
+
+    // TODO look up in config file
+    let class = "kafka-buffer".to_string();
+    let job_opts = JobOpts {
+        queue: "kafka-job-queue".to_string(),
+        ..Default::default()
+    };
     // Create the outer pipeline on the message stream.
     let stream_processor = consumer.stream().try_for_each(|borrowed_message| {
-        async move {
-            // Process each message
-            println!("{:?}", borrowed_message);
+        let r_body = borrowed_message
+            .payload()
+            .map_or(Ok("".to_string()), |bytes| {
+                String::from_utf8(bytes.to_vec())
+            });
+        async {
+            match r_body {
+                Err(err) => println!(
+                    "could not decode body as utf-8, skipping topic={} err={}",
+                    topic,
+                    err
+                ),
+                Ok(body) => {
+                    let job = Job {
+                        class: class.clone(),
+                        args: vec![sidekiq::Value::String(body)],
+                        retry: job_opts.retry,
+                        queue: job_opts.queue.clone(),
+                        jid: job_opts.jid.clone(),
+                        created_at: job_opts.created_at,
+                        enqueued_at: job_opts.enqueued_at,
+                    };
+                    match sidekiq_client.push_async(job).await {
+                        Ok(_) => (),
+                        Err(err) => println!("Sidekiq push failed: {}", err),
+                    }
+                }
+            }
             Ok(())
         }
     });
