1b0a67d881de9f7f51eafdee6bcf04e099dfd170
Daniel Bergey <daniel.bergey@braze.com>
2024-08-19 23:55:28 -0400

    topics lookup map, hardcoded for now

--- justfile
+++ justfile
@@ -1,6 +1,7 @@
 kafka: docker
     docker run -d -p 9092:9092 apache/kafka-native:3.8.0
-    kafka_2.13-3.8.0/bin/kafka-topics.sh --create --topic  buffer-topic --bootstrap-server localhost:9092
+    kafka_2.13-3.8.0/bin/kafka-topics.sh --create --topic  foo --bootstrap-server localhost:9092
+    kafka_2.13-3.8.0/bin/kafka-topics.sh --create --topic  bar --bootstrap-server localhost:9092
 
 docker:
     #!/usr/bin/env bash 
--- src/bin/producer.rs
+++ src/bin/producer.rs
@@ -1,4 +1,5 @@
 /// read from stdin, send each line to Kafka
+use std::collections::HashMap;
 use std::env;
 use std::time::Duration;
 
@@ -9,7 +10,7 @@ use http_body_util::{BodyExt, Empty};
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Request, Response};
+use hyper::{Request, Response, StatusCode};
 use hyper_util::rt::TokioIo;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
@@ -17,8 +18,8 @@ use tokio::net::TcpListener;
 #[derive(Clone, Debug)]
 struct Config {
     kafka_url: String,
-    topic: String,
     request_max_size: usize,
+    topics_map: HashMap<String, String>, // url path -> kafka topic
 }
 
 #[tokio::main]
@@ -29,39 +30,54 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .unwrap_or(SocketAddr::from(([0, 0, 0, 0], 3000)));
     let config: &'static Config = Box::leak(Box::new(Config {
         kafka_url: env::var("KAFKA_URL").unwrap_or("localhost:9092".to_string()),
-        topic: env::var("TOPIC").unwrap_or("buffer-topic".to_string()),
         request_max_size: env::var("REQUEST_MAX_SIZE")
             .ok()
             .and_then(|s| s.parse().ok())
             .unwrap_or(2 ^ 20),
+        topics_map: {
+            let mut topics = HashMap::new();
+            topics.insert("/foo".to_string(), "buffer-foo".to_string());
+            topics.insert("/bar".to_string(), "buffer-bar".to_string());
+            topics
+        },
     }));
 
     let write_to_kafka = move |req: Request<hyper::body::Incoming>| {
-        let req = http_body_util::Limited::new(req.into_body(), config.request_max_size);
         async {
-            match req.collect().await {
-                Ok(all) => {
-                    // Create the `FutureProducer` to produce asynchronously.
-                    let producer: FutureProducer = ClientConfig::new()
-                        .set("bootstrap.servers", &config.kafka_url)
-                        .set("message.timeout.ms", "1000")
-                        .create()?;
+            let path = req.uri().path();
+            match config.topics_map.get(path) {
+                None => Ok::<Response<Empty<Bytes>>, anyhow::Error>(
+                    Response::builder().status(StatusCode::NOT_FOUND).body(Empty::<Bytes>::new())?,
+                ),
+                Some(topic) => {
+                    let body =
+                        http_body_util::Limited::new(req.into_body(), config.request_max_size);
+                    match body.collect().await {
+                        Ok(all) => {
+                            // Create the `FutureProducer` to produce asynchronously.
+                            let producer: FutureProducer = ClientConfig::new()
+                                .set("bootstrap.servers", &config.kafka_url)
+                                .set("message.timeout.ms", "1000")
+                                .create()?;
 
-                    let mut v: Vec<u8> = Vec::new();
-                    v.extend(all.to_bytes().as_ref());
-                    let produce_future = producer.send(
-                        FutureRecord::<(), [u8]>::to(&config.topic)
-                            .payload(&v),
-                        Duration::from_secs(0),
-                    );
-                    match produce_future.await {
-                        Ok(delivery) => println!("Sent: {:?}", delivery),
-                        Err((e, _)) => println!("Error: {:?}", e),
+                            let mut v: Vec<u8> = Vec::new();
+                            v.extend(all.to_bytes().as_ref());
+                            let produce_future = producer.send(
+                                FutureRecord::<(), [u8]>::to(topic).payload(&v),
+                                Duration::from_secs(0),
+                            );
+                            match produce_future.await {
+                                Ok(delivery) => println!("Sent: {:?}", delivery),
+                                Err((e, _)) => println!("Error: {:?}", e),
+                            }
+                        }
+                        Err(err) => println!("error: {:?}", err),
                     }
+                    Ok::<Response<Empty<Bytes>>, anyhow::Error>(
+                        Response::new(Empty::<Bytes>::new()),
+                    )
                 }
-                Err(err) => println!("error: {:?}", err)
             }
-            Ok::<Response<Empty<Bytes>>, anyhow::Error>(Response::new(Empty::<Bytes>::new()))
         }
     };
 
