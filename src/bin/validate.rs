c62a4931c0241a46b199c13a81ec41b1b8316fd2
Daniel Bergey <daniel.bergey@braze.com>
2024-08-21 21:07:47 -0400

    parse idents, lists, defer schema

--- src/bin/validate.rs
+++ src/bin/validate.rs
@@ -6,55 +6,43 @@ use pest_derive::Parser;
 pub struct ConfigParser;
 
 fn print_parse(r: Rule, s: &str) {
-    println!("{:?}", ConfigParser::parse(r, s));
+    let r_parse = ConfigParser::parse(r, s);
+    println!("{:?}", r_parse);
+    if let Ok(mut parse) = r_parse {
+        if let Some(pair) = parse.next() {
+            println!(
+                "{:?} : {}",
+                pair.as_rule(),
+                pair.into_inner()
+                    .map(|p| format!("{:?} {}", p.as_rule(), p.as_span().as_str()))
+                    .collect::<Vec<String>>()
+                    .join(" ~ ")
+            )
+        }
+    }
 }
 
 fn print_parse_string(s: &str) {
     println!("{:?}", ConfigParser::parse(Rule::string, s));
 }
-
-fn validate(s: &str) -> Vec<String> {
-    let mut errors = Vec::new();
-    match ConfigParser::parse(Rule::config, s) {
-        Err(err) => errors.push(format!("{:?} {} {}", err.line_col, err.variant.message(), err.line())),
-        Ok(config) => {
-            for rule in config.into_inner() {
-                match rule.as_rule() {
-                    Rule::rule => {
-                        for term in rule.into_inner() {
-                            match term.as_rule() {
-                                Rule::string => (),
-                                Rule::attribute_set => {
-                                    let mut job_class = 0;
-                                    let mut queue = 0;
-                                    let mut topic = 0;
-                                    for attr in term {
-                                        match att.
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    _ => unreachable!(),
-                }
-            }
-        }
-    }
-   errors 
-}
-
 fn main() {
     print_parse_string("\"hello\"");
 
-    print_parse(Rule::attribute, "(job-class . \"Foo\")");
-    print_parse(Rule::attribute, "(queue . \"sidekiq_q_name\")");
-    print_parse(Rule::attribute, r#"(topic . "kafka_t_name")"#);
+    print_parse(Rule::pair, "(job-class . \"Foo\")");
+    print_parse(Rule::pair, "(queue . \"sidekiq_q_name\")");
+    print_parse(Rule::pair, r#"(topic . "kafka_t_name")"#);
+    print_parse(Rule::ident, r#"queue"#);
+    print_parse(Rule::ident, r#"job-class"#);
+    print_parse(Rule::list, r#"(no space)"#);
 
-    print_parse(Rule::attribute_set, r#"'(
+    print_parse(
+        Rule::list,
+        r#"'(
             (job-class . "Namespace::Foo")
             (queue . "foo_queue")
             (topic . "foo_topic")
-            )"#);
+            )"#,
+    );
 
     let unsuccessful_parse = ConfigParser::parse(Rule::string, "this is not a number");
     println!("{:?}", unsuccessful_parse);
